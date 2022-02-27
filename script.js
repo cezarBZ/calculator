1e21514097552a8a898011da2e11ea825bb36432
cezarBZ <cezar30santos@gmail.com>
2022-02-26 22:13:44 -0300

    criando funções de back e calculo

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const insert = (num) => {
 const cleanDisplay = () => {
     display.innerHTML = ''
 }
+
+const back = () => {
+    let result = display.innerHTML
+    display.innerHTML = result.substring(0, result.length -1);
+}
+
+const calc = () => {
+    let result = display.innerHTML
+    
+    if(result) {
+        display.innerHTML = eval(result)
+    }
+}
